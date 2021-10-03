74911a3e0a2db1e7706b55e1f0a6a44c45604b80
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-03 16:31:49 -0500

    Payment and Success layout

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
-  return <div>Success</div>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Hernán, gracias por pedido</h2>
+        <span>Tu pedido llegará en 3 días a tu dirección</span>
+        <div className="Success-map">Google maps</div>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
